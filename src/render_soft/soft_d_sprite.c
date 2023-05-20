e100edc713601ffd8565d7ac7fe8f50b47e921df
Ian Burgmyer <spectere@gmail.com>
2023-05-20 04:23:15 -0400

    Updated to compile under Linux.
    * GCC 13 appeasement (more dangling pointers, yay).
    * Accidental capital R ("src/rendeR_soft/blah")
    * Added necessary libm include.

--- src/render_soft/soft_d_sprite.c
+++ src/render_soft/soft_d_sprite.c
@@ -370,7 +370,7 @@ void D_DrawSprite(void) {
     int i, nump;
     float ymin, ymax;
     emitpoint_t *pverts;
-    sspan_t spans[MAXHEIGHT + 1];
+    static sspan_t spans[MAXHEIGHT + 1];
 
     sprite_spans = spans;
 
